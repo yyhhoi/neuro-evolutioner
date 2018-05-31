ddf58f889838b9338b5be23c5f74d76e1554e662
yyhhoi <yyhhoi@gmail.com>
2018-06-01 00:31:22 +0200

    Izhcikeich model and STDP

--- LIF_single.py
+++ LIF_single.py
@@ -20,31 +20,51 @@ class Connection(Network):
     def __init__(self, env, pre_neuron, post_neuron, tau_syn = 1, rev_potential = 0,g_syn = 1,
                  weight_ini = 1, x1_tau = 1, y1_tau = 1, y2_tau = 5,
                  weight_lr = 0.01, weight_decay_rate = 5, max_weight = 2, learning_enabled = True):
+        self.env = env
         self.pre_neuron = pre_neuron
         self.post_neuron = post_neuron
         self.x1_record, self.y1_record, self.y2_record, self.weight_record, self.syn_process_monitor = [], [], [], [], []
+        # Activate the increment
+        self.LTP_check = 0
+        self.LTD_check = 0
+
+        # Initialize events
+        self.LTP_allowed = self.env.event()
+        self.LTD_allowed = self.env.event()
+
         # Initialise the weight of the connection
         super(Connection, self).weights_map[self.pre_neuron.id, self.post_neuron.id] = weight_ini
+        
+        # Initialise parameters for the synapses
         self.tau_syn, self.rev_potential, self.g_syn = tau_syn, rev_potential, g_syn
+        
+        # Last spike time starts from negative infinity
         self.last_pre_spike_time = -9999
         self.last_post_spike_time = -9999
-        self.last_last_post_spike_time = -9999
-        self.pre_spike_detector = env.process(self.preSpikingListener())
-        self.post_spike_detector = env.process(self.postSpikingListener())
-        self.syncurrent_updater = env.process(self.synapticCurrent())
+        
+        # Initialise necessary processes for (1) spike detection and (2)update of synaptic current
+        self.pre_spike_detector = self.env.process(self.preSpikingListener())
+        self.post_spike_detector = self.env.process(self.postSpikingListener())
+        self.syncurrent_updater = self.env.process(self.synapticCurrent())
+        
+        # Enable synaptic plasticity
         if learning_enabled == True:
-            self.weight_incrementer = env.process(self.weightIncrementer(x1_tau, y1_tau, y2_tau,
-                                                                        weight_lr, weight_decay_rate, max_weight))
-        self.env = env
+            self.weight_incrementer = self.env.process(self.weightIncrementer(x1_tau, y1_tau, y2_tau,
+                                                                            weight_lr, weight_decay_rate, max_weight))
+            self.env.process(self.LTP_activated())
+            self.env.process(self.LTD_activated())
     def preSpikingListener(self):
         while True:
             yield self.pre_neuron.spikeOccur
             self.last_pre_spike_time = self.env.now
+            self.LTD_allowed.succeed()
+            self.LTD_allowed = self.env.event()
     def postSpikingListener(self):
         while True:
             yield self.post_neuron.spikeOccur
-            self.last_last_post_spike_time = self.last_post_spike_time
             self.last_post_spike_time = self.env.now    
+            self.LTP_allowed.succeed()
+            self.LTP_allowed = self.env.event()
     def synapticCurrent(self):
         while True:
             decay_kernel = np.exp(-(self.env.now - self.last_pre_spike_time)/self.tau_syn)
@@ -55,35 +75,51 @@ class Connection(Network):
     def weightIncrementer(self, x1_tau, y1_tau, y2_tau, weight_lr, weight_decay_rate, max_weight):
         while True:
             weight_this = super(Connection, self).getWeight(self.pre_neuron.id, self.post_neuron.id)
-            
             x1_trace = np.exp(-(self.env.now - self.last_pre_spike_time)/x1_tau)
             y1_trace = np.exp(-(self.env.now - self.last_post_spike_time)/y1_tau)
-            y2_trace = np.exp(-(self.env.now - self.last_last_post_spike_time)/y2_tau)
+            y2_trace = np.exp(-(self.env.now - self.last_post_spike_time)/y2_tau)
             # Record x1, y1, y2, weight 's data for analysis and plotting
             self.weight_record.append(weight_this)
             self.x1_record.append(x1_trace)
             self.y1_record.append(y1_trace)
             self.y2_record.append(y2_trace)
             delta_LTP = x1_trace * y2_trace
-            delta_LTD = y1_trace
-            # Attention!! Delta_weight should be discrete, and only defined at the moments of neuronal firing
-            # Correction of the bugs below is NEEDED!
-            # The traces should only be sampled at the points of update, but not continuously contribution
-            delta_weight = weight_lr * (max_weight - weight_this) * (delta_LTP - delta_LTD) - weight_decay_rate * weight_this
-            delta_weight = delta_weight * epsilon
+            delta_LTD = y1_trace*0.5
+            # LTP
+            if self.LTP_check == 1:
+                self.LTP_check = 0
+                delta_weight = weight_lr * (max_weight - weight_this) * (delta_LTP)
+                super(Connection, self).incrementWeightsMap(self.pre_neuron.id, self.post_neuron.id, delta_weight)
+            # LTD
+            if self.LTD_check == 1:
+                self.LTD_check = 0
+                delta_weight = weight_lr * (max_weight - weight_this) * (- delta_LTD)
+                super(Connection, self).incrementWeightsMap(self.pre_neuron.id, self.post_neuron.id, delta_weight)
+            # Natural decay
+            delta_weight = - weight_decay_rate * weight_this
             super(Connection, self).incrementWeightsMap(self.pre_neuron.id, self.post_neuron.id, delta_weight)
             yield self.env.timeout(epsilon)
-        pass
-
-        
-
+    def LTP_activated(self):
+        while True:
+            yield self.LTP_allowed
+            self.LTP_check = 1
+            yield self.env.timeout(epsilon)
+    def LTD_activated(self):
+        while True:
+            yield self.LTD_allowed
+            self.LTD_check = 1
+            yield self.env.timeout(epsilon)
         
 
 class Neuron(Network):
     count = 0 
     def __init__(self, env, name = "null"):
-        self.tau_membrane, self.u, self.u_rest, self.R, self.current = 1, -70, -70, 1, 0
-        self.u_threshold, self.u_reset, self.syncurrent = -40, -80, 0
+        # Parameters for passive membrane, threshold, reset, ... etc
+        self.tau_membrane, self.u, self.u_rest, self.R, self.current = 10, -70, -70, 1, 0
+        self.u_threshold, self.u_reset, self.syncurrent = 70, -80, 0
+        # Parameters for Izhikevich model
+        self.w, self.b, self.a, self.tau_w, self.u_critical = 0, 50, 0 , 5, -40
+        # Recording / Neuron's information
         self.somaV, self.spike_record  = [], []
         self.connected_to, self.connected_by = [], []
         self.name = name
@@ -96,14 +132,25 @@ class Neuron(Network):
         while True:
             self.somaV.append(self.u)
             self.syncurrent = super(Neuron,self).getPostCurrent(self.id)
-            dudt = - (self.u - self.u_rest) + self.R * self.current - self.syncurrent
+            # Main body of Differential Equations
+            dudt =  (self.u - self.u_rest) * (self.u - self.u_critical ) + self.R * self.current - self.syncurrent - self.R * self.w
+            # Integrate and fire model
+            # dudt =  -(self.u - self.u_rest) + self.R * self.current - self.syncurrent
             dudt = dudt/self.tau_membrane 
-            self.u += (dudt*epsilon)
+            dwdt = self.a * (self.u - self.u_rest) - self.w
+            dwdt = dwdt/self.tau_w
+
+            self.u += dudt * epsilon
+            self.w += dwdt * epsilon
+            # ^ Main body of Differential Equations
+            
             if self.u > self.u_threshold:
                 self.spike_record.append(( self.env.now, self.u))
                 self.u = self.u_reset
                 self.spikeOccur.succeed()
                 self.spikeOccur = env.event()
+                # After-spike update of Izhikevich model
+                self.w += self.b
             yield self.env.timeout(epsilon)
 
     def getSpikeRecord(self):
@@ -174,7 +221,7 @@ env.process(impulseU(env, in1, [5, 8]))
 env.process(impulseU(env, out1, [5.1, 6]))
 # Build connection. (env, presynaptic neuron, postsynaptic neuron)
 in1_out1 = Connection(env, in1, out1, rev_potential = 60, g_syn = 1, weight_ini = 0,
-                     x1_tau=5, y1_tau=0.01, y2_tau=5, weight_lr=1, weight_decay_rate = 0,  learning_enabled=True)
+                     x1_tau=5, y1_tau=5, y2_tau=5, weight_lr=0.3, weight_decay_rate = 0,  learning_enabled=True)
 
 neurons_list = [in1, out1]
 # Run the simulation
@@ -189,9 +236,11 @@ print("Total processes: %d" % len(Network.processes_monitor))
 axes = plot_soma(time, neurons_list)
 if len(axes) == 0:
     axes.set_xticks(np.arange(0, sim_time, step = 1))
+    # axes.set_ylim(-100,100)
 else:
     for i in range(len(axes)):
         axes[i].set_xticks(np.arange(0, sim_time, step = 1))
+        # axes[i].set_ylim(-100,100)
 
 f, axes2 = plt.subplots(1)
 axes2.plot(time, in1_out1.x1_record, label="x1")
