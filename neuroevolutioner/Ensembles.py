fix overflow + types_matrix's bug + reduce ranges of parameters + modified fitness

neuroevolutioner/Ensembles.py
@@ -31,7 +31,7 @@ class Ensemble_AdEx(object):
 
     def _membrane_potential_dyanmics_update(self):
 
-        non_linear_kernal = -(self.u - self.u_rest) + self.sharpness * np.exp((self.u-self.u_threshold)/self.sharpness)
+        non_linear_kernal = -(self.u - self.u_rest) + self.sharpness * np.exp((self.u-self.u_threshold)/(self.sharpness+1e-6))
         du_dt = (non_linear_kernal - self.r_m * self.w + self.r_m * self.I_ext - self.r_m * self.synaptic_currents)/self.tau_m
         
         dw_dt = (self.a * (self.u - self.u_rest) - self.w + (self.b * self.tau_w * self.firing_mask.get_mask()) )/self.tau_w
@@ -55,7 +55,7 @@ class Ensemble_AdEx(object):
 
     
     def _calc_synaptic_current(self):
-        u_2d = np.repeat(self.u.reshape(1,-1), self.num_neurons, axis=0)  # turn self.u to (1, num_neurons), then expand
+        u_2d = np.repeat(self.u.reshape(1,-1), self.num_neurons, axis=0)  # Expand row-wise
         current_2d = self.g * (u_2d - self.E_syn)
         current_1d = np.sum(current_2d, axis = 0)
         self.synaptic_currents = current_1d

neuroevolutioner/Genetics.py
@@ -214,8 +214,13 @@ class TL_FitnessMeasurer():
             output = np.sqrt(cov_mean)
         return output
     @staticmethod
-    def _jump_function(val, val_threshold, jump_target, fail_target = 0):
-        if val > val_threshold:
+    def _jump_function(val, val_threshold, jump_target, fail_target = 0, val_max = None):
+        condition = (val > val_threshold)
+        if val_max is None:
+            condition = (val > val_threshold)
+        else:
+            condition = ((val > val_threshold) and (val < val_max))
+        if condition:
             return jump_target
         else:
             return fail_target
@@ -227,18 +232,18 @@ class DA_FitnessMeasurer(TL_FitnessMeasurer):
         activity_np_related = np.array(self.activity_related)
         # S must have at least 1% of firing activity in the period of S
         least_S = np.mean(activity_np_related[0:int(self.accu_times["S"]/self.time_step), 0:self.anatomy_labels["sensory1"]])
-        S_least_score = self._jump_function(least_S, 0.01, 1, 0)
-        # B must have at least > 2 firing event over entire period
+        S_least_score = self._jump_function(least_S, val_threshold = 0.01, jump_target = 1)
+        # B must have 1% - 50% firing event over entire period
         B_activities = activity_np_related[:, self.anatomy_labels["sensory1"]:self.anatomy_labels["brain"]]
-        least_B = np.sum(B_activities)
-        B_least_score = self._jump_function(least_B, 2,  1, 0)
-        # A must have at least > 2 firing event
+        B_times, B_percent = np.sum(B_activities), np.mean(B_activities)
+        B_least_score = self._jump_function(B_percent, val_threshold = 0.01, jump_target = 1, val_max = 0.5)
+        # A must have at least > 2 firing event in the entired period
         A_activities = activity_np_related[:, self.anatomy_labels["brain"]:self.anatomy_labels["action1"]]
-        least_A = np.sum(A_activities)
-        A_least_score = self._jump_function(least_A, 2, 1, 0)
+        A_times = np.sum(A_activities)
+        A_least_score = self._jump_function(A_times, val_threshold = 2, jump_target = 1)
         # Delay score = (time of first onset of A firing) - (time of first onset of B firing)
         delay_score = 0
-        if (least_A > 0) and (least_B > 0):
+        if (A_times > 0) and (B_times > 0):
             A_fire_time = np.min(np.where(A_activities > 0)[0]) * self.time_step
             B_fire_time = np.min(np.where(B_activities > 0)[0]) * self.time_step
             delay_score = (A_fire_time - B_fire_time) * 2

neuroevolutioner/SynapticDynamics.py
@@ -138,7 +138,7 @@ class WeightsDynamics():
 
         """
         self._initialise_params(
-            anatomy = configs["anatomy_matrix"], types = configs["types_matrix"], w = configs["w"], w_max = configs["w_max"],
+            anatomy = configs["anatomy_matrix"], w = configs["w"], w_max = configs["w_max"], E_syn = configs["E_syn"], u_rest = configs["u_rest"],
             tau_LTP = configs["tau_LTP"], tau_LTP_slow = configs["tau_LTP_slow"], tau_LTD = configs["tau_LTD"], A = configs["A"],
             beta = configs["beta"], w_p = configs["w_p"], P = configs["P"], tau_cons = configs["tau_cons"],
             transmitter_constants = configs["transmitter_constants"],
@@ -148,7 +148,7 @@ class WeightsDynamics():
         pass
 
     def _initialise_params(self,
-                          anatomy, types, w, w_max,
+                          anatomy, w, w_max, E_syn, u_rest,
                           tau_LTP, tau_LTP_slow, tau_LTD, A,
                           beta, w_p, P, tau_cons,
                           transmitter_constants,
@@ -159,7 +159,8 @@ class WeightsDynamics():
         Args:
             A, B, beta, transmitter_constants, eta: recommended > 0
         """
-        self.w, self.w_max, self.anatomy, self.types = w, w_max, anatomy, types
+        self.w, self.w_max, self.anatomy = w, w_max, anatomy
+        self.types = (E_syn > np.repeat(u_rest.reshape(1,-1), u_rest.shape[0], axis = 0)).astype(int)
 
         # LTP/LTD
         self.z_LTP = SpikeTrace(self.num_neurons, tau_LTP, self.time_step)
@@ -174,7 +175,8 @@ class WeightsDynamics():
         self.transmitter_constants = transmitter_constants
         
         # Homeostatic
-        self.z_ht = SpikeTrace(self.num_neurons, tau_ht, self.time_step)
+        self.tau_ht = tau_ht
+        self.z_ht = SpikeTrace(self.num_neurons, self.tau_ht, self.time_step)
         self.tau_hom = tau_hom
         
         # Long-term inhibition